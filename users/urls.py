12fae6914cad986b55f3f2f1de2d41fea31935a3
Samet Sarıal <sarialsamet@gmail.com>
2021-12-06 00:27:30 +0300

    PUT api call for settings

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 
-from .views import   CustomUserDetailAPIView, UserViewSet
+from .views import   CustomUserDetailAPIView, CustomUserSettingsAPIView, UserViewSet
 
 urlpatterns = [
     path('', UserViewSet.as_view()),
     path('auth/', include('rest_auth.urls')),
     path('auth/register/', include('rest_auth.registration.urls')),
     #path('all', UserListView.as_view())
-    path('<int:pk>',CustomUserDetailAPIView.as_view(), name='kitap-bilgileri' ),
+    path('<int:pk>',CustomUserDetailAPIView.as_view(), name='user-information'),
+    path('setting/<int:pk>',CustomUserSettingsAPIView.as_view(), name='user-information1'),
 ]
